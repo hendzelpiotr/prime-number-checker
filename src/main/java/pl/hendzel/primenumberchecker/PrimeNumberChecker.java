7bafeaa20319442f4aab5afbac2c2dcc9eaf9346
Piotr Hendzel <phendzel.dev@gmail.com>
2021-05-27 20:26:37 +0200

    #3 - add basic impl to check if exception is thrown when input is not a number

--- src/main/java/pl/hendzel/primenumberchecker/PrimeNumberChecker.java
+++ src/main/java/pl/hendzel/primenumberchecker/PrimeNumberChecker.java
@@ -1,8 +1,13 @@
 package pl.hendzel.primenumberchecker;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class PrimeNumberChecker {
 
     public Boolean isPrimeNumber(String number) {
+        if (!StringUtils.isNumeric(number)) {
+            throw new IllegalArgumentException("Input is not a number");
+        }
         return null;
     }
 
